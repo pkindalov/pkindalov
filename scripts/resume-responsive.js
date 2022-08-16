f25e6498b60a3c83609239f8ec3c0b3561b7e1d9
pkindalov <panayotkindalov@gmail.com>
2022-08-16 10:01:29 +0300

    Small refactoring. Extracted js code to separate file - resume-responsive.js. Added functionality to flip page by press down left and right arrows

--- scripts/resume-responsive.js
+++ scripts/resume-responsive.js
@@ -1,11 +1,30 @@
-const flipbookEL = document.getElementById("flipbook");
+$(document).ready(() => {
+  const flipbookEL = document.getElementById("flipbook");
+  console.log(flipbookEL);
 
-window.addEventListener("resize", function (e) {
-  flipbookEL.style.width = "";
-  flipbookEL.style.height = "";
-  $(flipbookEL).turn("size", flipbookEL.clientWidth, flipbookEL.clientHeight);
-});
+  window.addEventListener("resize", function (e) {
+    flipbookEL.style.width = "";
+    flipbookEL.style.height = "";
+    $(flipbookEL).turn("size", flipbookEL.clientWidth, flipbookEL.clientHeight);
+  });
 
-$(flipbookEL).turn({
-  autoCenter: true,
+  window.addEventListener("keydown", (event) => {
+    const direction = event && event.key ? event.key.toLocaleLowerCase() : null;
+    switch (direction) {
+      case "arrowright":
+        $(flipbookEL).turn("next");
+        break;
+      case "arrowleft":
+        $(flipbookEL).turn("previous");
+        break;
+      default:
+        break;
+    }
+  });
+
+  $(flipbookEL).turn({
+    autoCenter: true,
+    width: 800,
+    height: 700,
+  });
 });
